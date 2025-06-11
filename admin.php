b61ded60fd0f7681a4f400af693960a49fc22921
cjreandino <cliceljeanereandino@gmail.com>
2025-06-11 18:36:15 +0800

    admin db connection

--- admin.php
+++ admin.php
@@ -1,3 +1,20 @@
+<?php
+    // Database configuration
+    $servername = "localhost";
+    $username = "root";
+    $password = ""; // **Security Note**: Store passwords securely (e.g., in environment variables)
+    $dbname = "scholarship";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $preview = "SELECT Appli_Num, CONCAT(FirstName, ' ', MidName, ' ', LastName, ' ', Suffix) AS FullName, Assistance_Type
+                FROM appli_details, appli_profile";
+    $adminview = $conn->query($preview);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,7 +38,21 @@
                 </thead>
                 <tbody id="applicantsTableBody">
                     <!-- Data will be populated from database -->
-                </tbody>
+                    <?php
+                    if ($adminview && $adminview->num_rows > 0) {
+                        while($row = $adminview->fetch_assoc()) {
+                            echo "<tr>
+                                    <td>{$row['Appli_Num']}</td>
+                                    <td>{$row['FullName']}</td>
+                                    <td>{$row['Assistance_Type']}</td>
+                                    <td><a href='view_applicant.php?id={$row['Appli_Num']}'>View</a></td>
+                                  </tr>";
+                        }
+                    } else {
+                        echo "<tr><td colspan='4'>No applicants found.</td></tr>";
+                    }
+                    ?>
+                    </tbody>
             </table>
         </div>
     </div>
